ff3ed5f8ca47c066f458dc4a7f049f53067165be
Quinn Vaughn <qvaughn3@gmail.com>
2018-01-10 10:31:43 -0600

    Re-setup config. Needs to get data now.

--- src/main/resources/site/src/components/App.js
+++ src/main/resources/site/src/components/App.js
@@ -10,157 +10,158 @@ import NavBar from './NavBar';
 import Main from './Main';
 import SettingsPage from './SettingsPage';
 import Login from './Login';
-import { auth } from './config/firebase';
+import { auth } from '../config/firebase';
 
 class App extends Component {
-  state = {
-    accounts: [],
-    isFollow: true,
-    user: null,
-    loaded: false,
-    isFetching: true
-  };
-  getFollow = () => {
-    let follow;
-    if (this.state.isFollow) {
-      follow = 'follow';
-    } else {
-      follow = 'unfollow';
-    }
-    return follow;
-  };
+	constructor(props) {
+		super(props);
+	}
+	state = {
+		accounts: [],
+		isFollow: true,
+		user: null,
+		loaded: false,
+		isFetching: true,
+	};
+	getFollow = () => {
+		let follow;
+		if (this.state.isFollow) {
+			follow = 'follow';
+		} else {
+			follow = 'unfollow';
+		}
+		return follow;
+	};
 
-  getUsername = () => {
-    return localStorage.getItem('username');
-  };
-  getTwitterAccounts = () => {
-    const follow = this.getFollow();
-    const username = this.getUsername();
-    axios
-      .get(
-        `https://twittercleaner-7476e.firebaseio.com/users/${username}/${
-          follow
-        }s.json`
-      )
-      .then(res => {
-        this.setState({ accounts: res.data, isFetching: false });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+	getUsername = () => {
+		return localStorage.getItem('username');
+	};
+	getTwitterAccounts = () => {
+		const follow = this.getFollow();
+		const username = this.getUsername();
+		axios
+			.get(
+				`https://twittercleaner-f1ecd.firebaseio.com/users/${username}/${follow}s.json`,
+			)
+			.then(res => {
+				this.setState({ accounts: res.data, isFetching: false });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
 
-  handleUserLogin = (user, accessToken, accessTokenSecret) => {
-    this.setState({
-      user: user,
-      accessToken: accessToken,
-      accessTokenSecret: accessTokenSecret
-    });
-    this.getTwitterAccounts();
-  };
+	handleUserLogin = (user, accessToken, accessTokenSecret) => {
+		this.setState({
+			user: user,
+			accessToken: accessToken,
+			accessTokenSecret: accessTokenSecret,
+		});
+		this.getTwitterAccounts();
+	};
 
-  handleUserLogout = () => {
-    auth.signOut().then(() => {
-      this.setState({ user: null, accessToken: '', accessTokenSecret: '' });
-    });
-  };
+	handleUserLogout = () => {
+		auth.signOut().then(() => {
+			this.setState({ user: null, accessToken: '', accessTokenSecret: '' });
+		});
+	};
 
-  handleRemove = userid => {
-    let newAccounts = this.state.accounts.slice();
-    const index = newAccounts.findIndex(a => a.userid === userid);
-    newAccounts.splice(index, 1);
-    this.setState({ accounts: newAccounts });
-  };
-  toggleClick = () => {
-    this.setState({
-      isFollow: !this.state.isFollow
-    });
-  };
-  componentWillMount() {
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        console.log(user);
-        this.setState({ user, loaded: true });
-      } else {
-        this.setState({ loaded: true });
-      }
-    });
-  }
+	handleRemove = userid => {
+		let newAccounts = this.state.accounts.slice();
+		const index = newAccounts.findIndex(a => a.userid === userid);
+		newAccounts.splice(index, 1);
+		this.setState({ accounts: newAccounts });
+	};
+	toggleClick = () => {
+		this.setState({
+			isFollow: !this.state.isFollow,
+		});
+	};
+	componentWillMount() {
+		auth.onAuthStateChanged(user => {
+			if (user) {
+				this.setState({ user, loaded: true });
+			} else {
+				this.setState({ loaded: true });
+			}
+		});
+	}
 
-  componentDidMount() {
-    if (localStorage.getItem('username') !== null) {
-      this.getTwitterAccounts();
-    }
-  }
+	componentDidMount() {
+		console.log(this.props.history);
+		if (localStorage.getItem('username') !== null) {
+			this.getTwitterAccounts();
+		}
+	}
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.isFollow !== this.state.isFollow) {
-      this.setState({ isFetching: true });
-      this.props.history.push(`${this.getFollow()}`);
-      this.getTwitterAccounts();
-    }
-  }
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.isFollow !== this.state.isFollow) {
+			this.setState({ isFetching: true });
+			this.props.history.push(`${this.getFollow()}`);
+			this.getTwitterAccounts();
+		}
+	}
 
-  render() {
-    const { accounts, isFollow, isFetching } = this.state;
-    if (!this.state.loaded) return <Loading />;
-    if (this.state.user === null || this.state.username === null) {
-      return (
-        <div>
-          <Redirect from="/" to="/login" />
-          <Route
-            exact={true}
-            path="/login"
-            render={props => (
-              <Login {...props} handleUserLogin={this.handleUserLogin} />
-            )}
-          />
-        </div>
-      );
-    } else {
-      return (
-        <div className={styles.App}>
-          <Container>
-            <NavBar
-              handleUserLogout={this.handleUserLogout}
-              user={this.state.user}
-            />
-            <Switch>
-              <Route path="/settings" component={SettingsPage} />
-              <Route
-                exact={true}
-                path="/follow"
-                render={props => (
-                  <Main
-                    {...props}
-                    toggle={this.toggleClick}
-                    isFetching={isFetching}
-                    accounts={accounts}
-                    isFollow={isFollow}
-                    handleRemove={this.handleRemove}
-                  />
-                )}
-              />
-              <Route
-                exact={true}
-                path="/unfollow"
-                render={props => (
-                  <Main
-                    {...props}
-                    toggle={this.toggleClick}
-                    isFetching={isFetching}
-                    accounts={accounts}
-                    isFollow={isFollow}
-                    handleRemove={this.handleRemove}
-                  />
-                )}
-              />
-              <Redirect from="/" to="/follow" />
-            </Switch>
-          </Container>
-        </div>
-      );
-    }
-  }
+	render() {
+		const { accounts, isFollow, isFetching } = this.state;
+		if (!this.state.loaded) return <Loading />;
+		if (this.state.user === null || this.state.username === null) {
+			return (
+				<div>
+					<Redirect from="/" to="/login" />
+					<Route
+						exact={true}
+						path="/login"
+						render={props => (
+							<Login {...props} handleUserLogin={this.handleUserLogin} />
+						)}
+					/>
+				</div>
+			);
+		} else {
+			return (
+				<div className={styles.App}>
+					<Container>
+						<NavBar
+							handleUserLogout={this.handleUserLogout}
+							user={this.state.user}
+						/>
+						<Switch>
+							<Route path="/settings" component={SettingsPage} />
+							<Route
+								exact={true}
+								path="/follow"
+								render={props => (
+									<Main
+										{...props}
+										toggle={this.toggleClick}
+										isFetching={isFetching}
+										accounts={accounts}
+										isFollow={isFollow}
+										handleRemove={this.handleRemove}
+									/>
+								)}
+							/>
+							<Route
+								exact={true}
+								path="/unfollow"
+								render={props => (
+									<Main
+										{...props}
+										toggle={this.toggleClick}
+										isFetching={isFetching}
+										accounts={accounts}
+										isFollow={isFollow}
+										handleRemove={this.handleRemove}
+									/>
+								)}
+							/>
+							<Redirect from="/" to="/follow" />
+						</Switch>
+					</Container>
+				</div>
+			);
+		}
+	}
 }
 export default withRouter(App);
--- src/main/resources/site/src/components/Login.jsx
+++ src/main/resources/site/src/components/Login.jsx
@@ -2,54 +2,52 @@ import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-import Firebase, { auth, provider } from './config/firebase';
 import styles from '../scss/Login.module.scss';
 import NavBar from './NavBar';
+import { auth, provider, database } from '../config/firebase';
 
 let appToken;
 
-const db = Firebase.database();
-
 export default class Login extends Component {
-  login = () => {
-    auth
-      .signInWithPopup(provider)
-      .then(res => {
-        const username = res.additionalUserInfo.username;
-        localStorage.setItem('username', username);
-        const user = res.user;
-        const accessToken = res.credential.accessToken;
-        const accessTokenSecret = res.credential.secret;
-        this.props.handleUserLogin(user, accessToken, accessTokenSecret);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-  render() {
-    return (
-      <div className={styles.body}>
-        <NavBar />
-        <img
-          className={styles.tweetImage}
-          src="http://www.mobyaffiliates.com/wp-content/uploads/2017/11/twitter-down.png"
-        />
-        <div className={styles.content}>
-          <h1>Welcome to Twitter Cleaner!</h1>
-          <br />
-          <br />
-          <h3>
-            Twitter Cleaner suggests Twitter users for you to follow and
-            unfollow<br />
-            based on your defined preferences.
-          </h3>
-          <p>To continue, please login with your Twitter Account.</p>
-          <Button onClick={this.login} className={styles.iconButton}>
-            <Icon name="twitter" size="large" className={styles.twitterIcon} />Log
-            In
-          </Button>
-        </div>
-      </div>
-    );
-  }
+	login = () => {
+		auth
+			.signInWithPopup(provider)
+			.then(res => {
+				const username = res.additionalUserInfo.username;
+				localStorage.setItem('username', username);
+				const user = res.user;
+				const accessToken = res.credential.accessToken;
+				const accessTokenSecret = res.credential.secret;
+				this.props.handleUserLogin(user, accessToken, accessTokenSecret);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+	render() {
+		return (
+			<div className={styles.body}>
+				<NavBar />
+				<img
+					className={styles.tweetImage}
+					src="http://www.mobyaffiliates.com/wp-content/uploads/2017/11/twitter-down.png"
+				/>
+				<div className={styles.content}>
+					<h1>Welcome to Twitter Cleaner!</h1>
+					<br />
+					<br />
+					<h3>
+						Twitter Cleaner suggests Twitter users for you to follow and
+						unfollow<br />
+						based on your defined preferences.
+					</h3>
+					<p>To continue, please login with your Twitter Account.</p>
+					<Button onClick={this.login} className={styles.iconButton}>
+						<Icon name="twitter" size="large" className={styles.twitterIcon} />Log
+						In
+					</Button>
+				</div>
+			</div>
+		);
+	}
 }
